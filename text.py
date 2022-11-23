cf579825a98da450b7c1e589d1f4c6701b39dcc3
kakala666 <byby40@163.com>
2022-11-23 13:24:15 +0800

    ~

--- text.py
+++ text.py
@@ -1,4 +1,7 @@
 from dingtalkchatbot.chatbot import DingtalkChatbot,ActionCard,CardItem
-def talktext(webhook,secret,body):
-    xiaoding = DingtalkChatbot(webhook, secret=secret)
+def talktext(wehook,secret,body):
+    xiaoding = DingtalkChatbot(wehook, secret=secret)
     xiaoding.send_text(msg=body, is_at_all=False)
+def talklink(wehook,secret,titel,text,Url,photo_URL):
+    xiaoding = DingtalkChatbot(wehook,secret=secret)
+    xiaoding.send_link(titel=titel,text=text,message_url==Url,pic_url=photo_URL)
